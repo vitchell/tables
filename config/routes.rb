225a68bf38d2d82bd448f6831439570359742a2d
Jason B <devilshaircut@gmail.com>
2013-11-24 02:38:28 -0500

    Add in missing route for selecting a table. It is available at /step031.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Tables::Application.routes.draw do
   get 'step01' => "home#step01"
   get 'step02' => "home#step02"
   get 'step03' => "home#step03"
+  get 'step031' => "home#step031"
   get 'step04' => "home#step04"
   get 'step05' => "home#step05"
   get 'step06' => "home#step06"
