128cf05b1717f89ef7f229c0caa6a96dfe1ad1fb
Jason B <devilshaircut@gmail.com>
2013-11-23 19:07:32 -0500

    Add in club list page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,21 @@
 class HomeController < ApplicationController
+
+  def index
+  end
+
+  def step01
+  end
+
+  def step02
+  end
+
+  def step03
+  end
+
+  def step04
+  end
+
+  def step05
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Tables::Application.routes.draw do
   root 'home#index'
+  
   get 'iphone' => "home#iphone"
+
+  get 'step01' => "home#step01"
+  get 'step02' => "home#step02"
+  get 'step03' => "home#step03"
+  get 'step04' => "home#step04"
+  get 'step05' => "home#step05"
+
 end
