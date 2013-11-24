041482db03d31961c9912ad58972452c0890df76
Mitchell Lane <mitchell@fullscreen.net>
2013-11-24 03:03:28 -0500

    Presentation

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,15 @@ Tables::Application.routes.draw do
   get 'step07' => "home#step07"
   get 'step08' => "home#step08"
 
+  get 'slide1' => "presentation#slide1"
+  get 'slide2' => "presentation#slide2"
+  get 'slide3' => "presentation#slide3"
+  get 'slide4' => "presentation#slide4"
+  get 'slide5' => "presentation#slide5"
+  get 'slide6' => "presentation#slide6"
+  get 'slide7' => "presentation#slide7"
+  get 'slide8' => "presentation#slide8"
+  get 'slide9' => "presentation#slide9"
+  get 'slide10' => "presentation#slide10"
+
 end
